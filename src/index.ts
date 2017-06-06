6aae8c51db5fd8583e749fee7a9ae7de96033f19
Ron Winacott <ron.winacott@securekey.com>
2017-06-06 12:15:06 -0400

    Added code comments and corrected spelling again.

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * This server will echo everything it receives back to the caller of the mapped interface.
  * This is to simulate the downstream system of the Matching Service.
  * Only the GET method is implemented. All other REST methods will return a 500 error.
- * There are some key data elements you can pass to cuase the system to simulate
+ * There are some key data elements you can pass to cause the system to simulate
  * an error condition if needed for test the calling matching service.
  * 
  * @author Ron Winacott <ron.winacott@securekey.com>
@@ -11,8 +11,9 @@
  *
  */
 
-//console.log("Starting the Echo REST Server.");
-
+/*
+ * Start of main code here
+ */
 import * as http from 'http';
 import * as debug from 'debug';
 
@@ -29,13 +30,35 @@ console.log("Starting the Echo Server at http://localhost:"+port);
 server.on('error', onError);
 server.on('listening', onListening);
 
+// ----------- End of main code -----------------
+
+
+/**
+ * Get the port number from the environment, configuration or fallback
+ * default.
+ * 
+ * @param {(number|string)} val 
+ * @returns {(number|string|boolean)} 
+ */
 function normalizePort(val: number|string): number|string|boolean {
   let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
-  if (isNaN(port)) return val;
-  else if (port >= 0) return port;
-  else return false;
+  if (isNaN(port)) {
+    return val;
+  }
+  else if (port >= 0) {
+    return port;
+  }
+  else {
+    return false;
+  }
 }
 
+
+/**
+ * Error handler for the express Application
+ * 
+ * @param {NodeJS.ErrnoException} error 
+ */
 function onError(error: NodeJS.ErrnoException): void {
   if (error.syscall !== 'listen') throw error;
   let bind = (typeof port === 'string') ? 'Pipe ' + port : 'Port ' + port;
@@ -53,6 +76,11 @@ function onError(error: NodeJS.ErrnoException): void {
   }
 }
 
+
+/**
+ * The listening event handler for the system.
+ * 
+ */
 function onListening(): void {
   let addr = server.address();
   let bind = (typeof addr === 'string') ? `pipe ${addr}` : `port ${addr.port}`;
